6c2984013f48341ecf47c3be07e0e44d63d146c5
Rodolfo <chavesrodolfo@gmail.com>
2019-02-10 12:59:53 -0200

    componente tweet e timeline

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import api from '../services/api'
+import socket from 'socket.io-client'
 
 import twitterLogo from '../twitter.svg';
 import './Timeline.css';
 
-import api from '../services/api';
+import Tweet from '../components/Tweet'
 
 export default class Timeline extends Component {
 
   state = {
+    tweets: [],
     newTweet: ''
   };
+
+  // Método herdado do Component que é executado quando o componente é criado (exibido em tela)
+  async componentDidMount() {
+    this.subscribeToEvents()
+
+    const response = await api.get('tweets')
+    this.setState({
+      tweets: response.data
+    })
+  }
+
+  subscribeToEvents = () => {
+    const io = socket('http://localhost:3000')
+
+    io.on('tweet', data => {
+      this.setState({
+        // coloca o tweet na primeira posicao do array e copia todos os outros da lista para as demais posições
+        tweets: [data, ...this.state.tweets]
+      });
+    });
+
+    io.on('like', data => {
+      this.setState({
+        //percorre os tweets, procura o que precisa ser atualizado e atualiza
+        tweets: this.state.tweets.map(tweet => 
+          tweet._id === data._id ? data : tweet)
+      });
+    });
+  }
 
   handleInputChange = e => {
     this.setState({ newTweet: e.target.value });
@@ -36,6 +68,13 @@ export default class Timeline extends Component {
             onKeyDown={this.handleNewTweet}
             placeholder="O que está acontecendo?" />
         </form>
+
+        <ul className="tweet-list">
+          {this.state.tweets.map(tweet => (
+            <Tweet key={tweet._id} tweet={tweet} />
+          ))}
+        </ul>
+
       </div>
     )
   }
